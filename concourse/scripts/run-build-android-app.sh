b4b08a786d4cffd75b4750cdadeae2be805b328c
AmitHule <33020173+AmitHule@users.noreply.github.com>
2017-12-15 16:44:31 +0530

    Update run-build-android-app.sh

--- concourse/scripts/run-build-android-app.sh
+++ concourse/scripts/run-build-android-app.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -e
 
-cd ios-repo/mechanic
+cd android-repo/concourse
 
-carthage update --platform iOS --no-use-binaries
-
-xcodebuild -project mechanic.xcodeproj -scheme mechanic -sdk iphoneos -archivePath "$ARCHIVE_FILENAME" -configuration AdHoc archive
-xcodebuild -exportArchive -archivePath "$ARCHIVE_FILENAME" -exportPath build -exportOptionsPlist "Resources/ExportOptions.plist"
+./gradlew build
