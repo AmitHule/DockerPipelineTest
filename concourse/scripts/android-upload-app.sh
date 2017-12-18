6120b1620a567098973641517c78b09cc6921a23
Raj Singh <Raj.krishan.singh@volkswagen.co.in>
2017-12-18 12:06:01 +0530

    cheanges to add sh

--- concourse/scripts/android-upload-app.sh
+++ concourse/scripts/android-upload-app.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+APK_FILEPATH="apk=@built-android-app/$IPA_FILENAME"
+HOCKEY_REQUEST_HEADER="X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+
+ls built-ios-app/
+
+response=$(curl \
+  -F "status=2" \
+  -F "notify=0" \
+  -F "notes= $notes" \
+  -F "notes_type=0" \
+  -F "mandatory=0" \
+  -F "$IPA_FILEPATH" \
+  -H "$HOCKEY_REQUEST_HEADER" \
+https://rink.hockeyapp.net/api/2/apps/upload)
+
+if ! [[ "$response" == *"created_at"* ]]; then
+	exit 1
+fi
--- concourse/scripts/run-build-android-app.sh
+++ concourse/scripts/run-build-android-app.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
 
 ./gradlew build
+cp android-repo/app/build/outputs/apk/app-debug.apk built-android-app/
+pwd
