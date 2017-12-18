642863069df6893d4f5824b69ea778a760240382
Raj Singh <Raj.krishan.singh@volkswagen.co.in>
2017-12-18 13:08:21 +0530

    cheanges to add sh

--- concourse/scripts/run-build-android-app.sh
+++ concourse/scripts/run-build-android-app.sh
@@ -3,8 +3,5 @@
 ./gradlew build
 pwd
 chmod 777 ../built-androiod-app
-
-cd ../built-androiod-app
-pwd
-cp app/build/outputs/apk/app-debug.apk ../built-android-app/
+cp app/build/outputs/apk/app-debug.apk ../built-android-app
 
