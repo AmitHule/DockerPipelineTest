46308914a89ec81a994d714962be5bc48ad9a929
Raj Singh <Raj.krishan.singh@volkswagen.co.in>
2017-12-18 13:20:32 +0530

    cheanges to add sh

--- concourse/scripts/android-upload-app.sh
+++ concourse/scripts/android-upload-app.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-APK_FILEPATH="apk=@built-android-app/$IPA_FILENAME"
+APK_FILEPATH="apk=@built-android-app/$APK_FILENAME"
 HOCKEY_REQUEST_HEADER="X-HockeyAppToken: $HOCKEY_APP_TOKEN"
 
-ls built-ios-app/
+ls built-android-app/
 
 response=$(curl \
   -F "status=2" \
   -F "notify=0" \
-  -F "notes= $notes" \
+  -F "notes= test" \
   -F "notes_type=0" \
   -F "mandatory=0" \
-  -F "$IPA_FILEPATH" \
+  -F "$APK_FILEPATH" \
   -H "$HOCKEY_REQUEST_HEADER" \
 https://rink.hockeyapp.net/api/2/apps/upload)
 
